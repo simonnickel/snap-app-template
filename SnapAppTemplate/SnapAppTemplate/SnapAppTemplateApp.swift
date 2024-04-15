25c1f8b0913420176b3f7f8e82bc409f07690752
Simon Nickel <simonnickel@googlemail.com>
2024-04-15 13:33:01 +0200

    Create project with Xcode 15.3.0

---
+++ SnapAppTemplate/SnapAppTemplate/SnapAppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapAppTemplateApp.swift
+//  SnapAppTemplate
+//
+//  Created by Simon Nickel on 15.04.24.
+//
+
+import SwiftUI
+
+@main
+struct SnapAppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
