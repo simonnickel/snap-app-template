c63669eaf72233b36258dc9976bcecc82e3c3bac
Simon Nickel <simonnickel@googlemail.com>
2024-04-22 21:55:19 +0200

    Fix and rename

---
+++ SnapAppTemplate/SnapAppTemplate/Theme/NumberKeys+App.swift
@@ -0,0 +1,29 @@
+//
+//  NumberKeys+App.swift
+//  SnapAppTemplate
+//
+//  Created by Simon Nickel on 03.09.23.
+//
+
+import SwiftUI
+import SnapTheme
+
+extension Theme.NumberKey {
+	
+	
+	// MARK: - Base overrides
+	
+	static var definitionsApp: [Theme.NumberKey : Theme.NumberValue] {[
+		
+		:
+		
+	]}
+	
+	
+	// MARK: - App Keys
+	
+	public static let allAppKeys: [Theme.NumberKey] = [.exampleNumber]
+	
+	public static let exampleNumber = Theme.NumberKey("exampleNumber", default: .value(10))
+	
+}
