7b9e7134e4532b6839b1bc090930e7ad328372bb
Simon Nickel <simonnickel@googlemail.com>
2024-10-09 20:27:26 +0200

    Remove environment workaround

--- SnapAppTemplate/SnapAppTemplate/AppStructure/AppEnvironment.swift
+++ SnapAppTemplate/SnapAppTemplate/AppStructure/AppEnvironment.swift
@@ -5,43 +5,11 @@
 
 import SwiftUI
 
-/*
-// MARK: - MyType
-
-private struct MyTypeKey: EnvironmentKey {
-	static let defaultValue: MyType = MyType()
-}
-
-extension EnvironmentValues {
-
-	var myType: MyType {
-		get { self[MyTypeKey.self] }
-		set { self[MyTypeKey.self] = newValue }
-	}
-	
-}
-*/
-
 
 // MARK: - Data Source
 
-private struct DataSourceKey: EnvironmentKey {
-	
-	static var defaultValue: DataSource {
-		// TODO concurrency: Instead of a `static let` this needs to be a computed property. Just applying @MainActor does not fullfill requirements for `EnvironmentKey` implementation.
-		// Not sure if this might be something that is solved in a future version of Swift or SwiftUI.EnvironmentKey.
-		// Static property 'defaultValue' is not concurrency-safe because it is not either conforming to 'Sendable' or isolated to a global actor; this is an error in Swift 6
-		// Main actor-isolated static property 'defaultValue' cannot be used to satisfy nonisolated protocol requirement
-		DataSource()
-	}
-	
-}
-
 extension EnvironmentValues {
 	
-	var dataSource: DataSource {
-		get { self[DataSourceKey.self] }
-		set { self[DataSourceKey.self] = newValue }
-	}
+	@Entry var dataSource: DataSource = DataSource()
 	
 }
