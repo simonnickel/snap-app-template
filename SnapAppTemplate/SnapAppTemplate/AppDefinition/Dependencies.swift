a51486771fcccd2f5a8b62cd8caab09a585f73a1
Simon Nickel <simonnickel@googlemail.com>
2024-11-05 23:16:04 +0100

    Cleanup DependencyForwardingFactory

--- SnapAppTemplate/SnapAppTemplate/AppDefinition/Dependencies.swift
+++ SnapAppTemplate/SnapAppTemplate/AppDefinition/Dependencies.swift
@@ -10,3 +10,27 @@ extension Dependencies {
 	var dataSource: DataSource { DataSource() }
 	
 }
+
+
+// MARK: - DependencyForwardingFactory
+
+/// Allows to provide an implementation for a dependency that is defined in a package.
+/// Define KeyPath in a package:
+/// ```
+/// var service: Service { Dependencies.forwarding(for: \.service) }
+/// ```
+/// Provide implementation below.
+extension Dependencies: @retroactive DependencyForwardingFactory {
+	
+	public func create<Dependency>(for keyPath: KeyPath<Dependencies, Dependency>) -> Dependency? {
+		switch keyPath {
+			
+			/// KeyPath `\.service` is defined in package, Implementation `Service()` is in project.
+			// case \.service: Service() as? Dependency
+				
+			default: nil
+
+		}
+	}
+	
+}
