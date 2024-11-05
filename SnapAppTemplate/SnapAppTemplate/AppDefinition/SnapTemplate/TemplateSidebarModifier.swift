1263ba698de726bc99bf8e13270ea075c6c9fd8c
Simon Nickel <simonnickel@googlemail.com>
2024-11-05 23:25:18 +0100

    Cleanup setup modifier

--- SnapAppTemplate/SnapAppTemplate/AppDefinition/SnapTemplate/TemplateSidebarModifier.swift
+++ SnapAppTemplate/SnapAppTemplate/AppDefinition/SnapTemplate/TemplateSidebarModifier.swift
@@ -6,12 +6,10 @@
 import SwiftUI
 
 internal struct TemplateSidebarModifier : ViewModifier {
-
-	@Environment(Navigator.self) private var navigator
 	
 	internal func body(content: Content) -> some View {
 		content
-			.environment(\.navigatorSettings, TemplateSettingsDestinationTranslator(navigator: navigator))
+			.setupTemplateSettingsNavigator()
 	}
 	
 }
